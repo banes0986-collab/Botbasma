ee5a4129ad52f25c0c595cb73c1ed48c2b54fdf5
banes0986-collab <banes0986@gmail.com>
2026-03-28 18:36:31 +0400

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,13 @@
+{
+  "name": "trigger-vip-engine",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mineflayer": "^4.15.0",
+    "cors": "^2.8.5"
+  }
+}
