d6811d98085df06e2a7b9f9f20010b8eaef4673e
banes0986-collab <banes0986@gmail.com>
2026-03-28 20:06:20 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const app = express();
-const port = 3000;
-
 const mc = require("minecraft-server-util");
 
+const app = express();
 app.use(express.static("public"));
 
 app.get("/status", async (req, res) => {
     const ip = req.query.ip;
+
     try {
         const result = await mc.status(ip, 25565);
+
         res.json({
             online: true,
             players: result.players.online,
             max: result.players.max,
+            ping: result.roundTripLatency,
             motd: result.motd.clean
         });
-    } catch (e) {
+
+    } catch {
         res.json({ online: false });
     }
 });
 
-app.listen(port, () => {
-    console.log("Server başladı");
-});
+app.listen(3000, () => console.log("Panel işləyir 🔥"));
