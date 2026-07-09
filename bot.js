d21418d6d7c68bce17b84357a67c90774470049f
banes0986-collab <banes0986@gmail.com>
2026-07-09 15:58:31 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+// bot.js içindeki createMinecraftBot fonksiyonunun ayar kısmı:
+const bot = mineflayer.createBot({
+    host: 'sunucu-adresiniz.aternos.me', 
+    port: 25565,                         
+    username: 'LegacyBot_724', // Botun oyundaki adı artık LegacyBot
+    version: '1.20.1'                    
+});
